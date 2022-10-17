3f5f03cd35945b5ad71067420a0d142804c8007f
samchi72 <leonftto@gmail.com>
2022-10-17 23:39:52 +0000

    arquivos gasolina.png e gasolina.py alterados

--- gasolina.py
+++ gasolina.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 gasolina_df = pd.read_csv('gasolina.csv', sep=',')
 
-with sns.axes_style('whitegrid'):
+with sns.axes_style('darkgrid'):
   grafico_gasolina = sns.lineplot(data=gasolina_df, x='dia', y='venda')
-  grafico_gasolina.set(title='Preço da gasolina nos 10 primeiros dias de Julho de 2021', xlabel='dia', ylabel='preço')
+  grafico_gasolina.set(title='Gasoline prices in São Paulo on the first 10 days of July 2021', xlabel='day', ylabel='price')
   plt.savefig('gasolina.png')
